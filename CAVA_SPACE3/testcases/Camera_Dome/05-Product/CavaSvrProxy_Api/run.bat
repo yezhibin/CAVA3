d9c16b6b272cc69289c3b8f801d294650b4e00e0
zoudaohoutian <yzbemail@sina.cn>
2022-03-15 00:32:12 +0800

    [MOD][NORMAL]增加cava3svrproxy接口用例

---
+++ CAVA_SPACE3/testcases/Camera_Dome/05-Product/CavaSvrProxy_Api/run.bat
@@ -0,0 +1,9 @@
+set CAVA_SPACE=D:\CAVA3\CAVA_SPACE3
+set PYTHONPATH=D:\CAVA3\CAVA_SPACE3\src
+d:
+cd %CAVA_SPACE%
+cd %CAVA_SPACE%\testcases
+cd %CAVA_SPACE%\testcases\Camera_Dome
+cd %CAVA_SPACE%\testcases\Camera_Dome\05-Product
+cava client_no_limit=5 execution_plan=first_var testdatas=D:\CAVA3\CAVA_SPACE3\testcases\Camera_Dome\05-Product\CavaSvrProxy_Api\DeviceList.xls CavaSvrProxy_Api\src
+pause
